55da71cc961a54e3141ef21c1d419959a38ef99a
守军 潘 <175979885@qq.com>
2019-02-26 14:07:43 +0800

    signalr

---
+++ SignalR/SignalR/Common/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Common
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        /// <summary>
+        /// 供客户端调用的服务器端代码
+        /// </summary>
+        /// <param name="message"></param>
+        public void Send(string message)
+        {
+            var name = Guid.NewGuid().ToString().ToUpper();
+            // 调用所有客户端的sendMessage方法
+            Clients.All.sendMessage(name, message);
+        }
+
+        /// <summary>
+        /// 客户端连接的时候调用
+        /// </summary>
+        /// <returns></returns>
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+    }
+}
